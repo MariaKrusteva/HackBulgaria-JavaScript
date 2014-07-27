10f6ae9f00d09dc258c3d1defa9d858f9d510d11
MariaKrusteva <mimito_68@abv.bg>
2014-07-27 16:28:16 +0300

    fixed issue

--- exam2/1-Corrupted-Data/corruptedData.js
+++ exam2/1-Corrupted-Data/corruptedData.js
@@ -1,17 +1,23 @@
 "use strict";
 
-var data = require("./data");
-var _ = require("lodash");
+var
+  data = require("./data"),
+  _ = require("lodash");
 
-var corruptedData = function(data){
-  var result = [];
-  var grouped = _.groupBy(data, function(item){return item.fields.student + "-" + item.fields.date;});
+function corruptedData(data){
+  var
+    result = [],
+    grouped = _.groupBy(data, function(item){
+      return item.fields.student + "-" + item.fields.date;
+    });
+
   Object.keys(grouped).forEach(function(key){
     if(grouped[key].length > 1){
       grouped[key].shift();
       result.push(grouped[key]);
     }
   });
+
   return _.flatten(result);
 };
 
